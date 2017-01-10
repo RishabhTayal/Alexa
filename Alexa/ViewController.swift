1fe2258d7cd3f65fd24dc9dfb9649d3ba961b012
Rishabh Tayal <rtayal11@gmail.com>
2017-01-10 17:19:42 -0600

    Initial Commit

---
+++ Alexa/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Alexa
+//
+//  Created by Tayal, Rishabh on 1/10/17.
+//  Copyright © 2017 Tayal, Rishabh. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
